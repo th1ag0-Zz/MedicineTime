bc0e5bfd4932c49856bde4e165776c0146ffa8de
Thiago <thiagoF89.tf@gmail.com>
2021-05-11 12:29:09 -0300

    add: routes

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Welcome from '../pages/Welcome';
+
+const stackRoutes = createStackNavigator()
+
+const AppRoutes: React.FC = () => {
+  return (
+    <stackRoutes.Navigator
+      headerMode="none"
+    >
+      <stackRoutes.Screen
+        name="Welcome"
+        component={Welcome}
+      />
+    </stackRoutes.Navigator>
+  )
+}
+
+export default AppRoutes;
