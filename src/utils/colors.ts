ece0138d234ff9e2e4add0e9cfb8106ba4885a14
Thiago <thiagoF89.tf@gmail.com>
2021-05-11 15:19:15 -0300

    add: colors

---
+++ src/utils/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+  green_dark: '#264653',
+  green: '#2a9d8f',
+  orange_light: '#e9c46a',
+  orange_dark: '#f4a261',
+  red_light: '#e76f51',
+  red: '#E83F5B',
+  text_heading: '#52665A',
+  text: '#5C6660'
+}
+
+export default colors;
